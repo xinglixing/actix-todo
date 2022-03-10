c2220a139494e1fae197175a08d1ac266d2e2c20
Xing Li <ilgnix.0211@icloud.com>
2022-03-10 13:07:44 -0800

    Add env config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod models;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use models::Status;
+
+async fn status() -> impl Responder {
+    HttpResponse::Ok().json(Status {
+        status: "Ok".to_string(),
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let config = crate::config::Config::from_env().unwrap();
+
+    println!(
+        "Starting server {}:{}",
+        config.server.host, config.server.port
+    );
+
+    HttpServer::new(|| App::new().route("/", web::get().to(status)))
+        .bind((config.server.host, config.server.port))?
+        .run()
+        .await
 }

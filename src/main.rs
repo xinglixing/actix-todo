7de7cf8df37b2989192b18761ac315325c07be22
Xing Li <ilgnix.0211@icloud.com>
2022-03-10 13:47:52 -0800

    Add postgres pool

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod config;
 mod models;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use deadpool_postgres::Runtime;
 use dotenv::dotenv;
 use models::Status;
+use tokio_postgres::NoTls;
 
 async fn status() -> impl Responder {
     HttpResponse::Ok().json(Status {
@@ -16,13 +18,19 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let config = crate::config::Config::from_env().unwrap();
 
+    let pool = config.pg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+
     println!(
         "Starting server {}:{}",
         config.server.host, config.server.port
     );
 
-    HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind((config.server.host, config.server.port))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(pool.clone())
+            .route("/", web::get().to(status))
+    })
+    .bind((config.server.host, config.server.port))?
+    .run()
+    .await
 }
